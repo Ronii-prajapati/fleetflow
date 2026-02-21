70ec4fb8a53d9bcb2d6383f6ff0fa772caf6ce2b
arpitvarde612 <arpitvarde9999@gmail.com>
2026-02-21 12:09:18 +0530

    Update database.py

---
+++ app/core/database.py
@@ -0,0 +1,11 @@
+import mysql.connector
+from app.core.config import settings
+
+def get_db():
+    return mysql.connector.connect(
+        host=settings.DB_HOST,
+        port=settings.DB_PORT,
+        user=settings.DB_USER,
+        password=settings.DB_PASSWORD,
+        database=settings.DB_NAME
+    )
