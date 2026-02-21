7c1f03b0ca239b406ee62ea06b910b2a77c5da34
arpitvarde612 <arpitvarde9999@gmail.com>
2026-02-21 13:30:31 +0530

    Update database.py

--- app/core/database.py
+++ app/core/database.py
@@ -1,5 +1,8 @@
 import mysql.connector
 from app.core.config import settings
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker,declarative_base
+from app.core.config import settings
 
 def get_db():
     return mysql.connector.connect(
@@ -9,3 +12,14 @@ def get_db():
         password=settings.DB_PASSWORD,
         database=settings.DB_NAME
     )
+
+URL=f"mysql+mysqlconnector://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}/{settings.DB_NAME}"
+
+engine=create_engine(URL)
+SessionLocal=sessionmaker(bind=engine)
+Base=declarative_base()
+
+def get_db():
+    db=SessionLocal()
+    try: yield db
+    finally: db.close()
